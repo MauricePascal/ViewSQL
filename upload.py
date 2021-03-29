8ba07d95c3174812763239af82c95d789c37f1d1
Maurice-Pascal L <larivieremauricepascal112@gmail.com>
2021-03-29 20:51:11 +0200

    Delete upload.py

--- upload.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-version = sys.argv[1]
-os.system("cmd /c git fetch")
-os.system("cmd /c git pull")
-os.system("cmd /c git add .")
-os.system(f"cmd /c git commit -m \"Added {version}\"")
-os.system("cmd /c git push -u origin downloads")
