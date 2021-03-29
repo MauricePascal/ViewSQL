e4dec2051687159c2fd3fc1a0b2ca37a219b0a63
MauricePascal <larivieremauricepascal112@gmail.com>
2021-03-29 20:03:20 +0200

    Made some changes

--- upload.py
+++ upload.py
@@ -1,9 +1,12 @@
 import sys
 import os
+import time
 
 version = sys.argv[1]
-os.system("cmd /c git fetch")
-os.system("cmd /c git pull")
+os.system("cmd /c git pull origin downloads")
+time.sleep(1)
 os.system("cmd /c git add .")
+time.sleep(1)
 os.system(f"cmd /c git commit -m \"Added {version}\"")
+time.sleep(1)
 os.system("cmd /c git push -u origin downloads")
